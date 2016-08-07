21d45931642756593fb80e42ccd357c44b2bf97e
liumeng <378338627@qq.com>
2016-08-07 22:26:22 +0800

    20160802226
    20160802226

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','starter.filters'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','starter.filters','ionic-native-transitions'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -28,7 +28,32 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','s
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider,$sceDelegateProvider) {
+.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider,$sceDelegateProvider,$ionicNativeTransitionsProvider) {
+
+  $ionicNativeTransitionsProvider.setDefaultOptions({
+    duration: 400, // in milliseconds (ms), default 400,
+    slowdownfactor: 4, // overlap views (higher number is more) or no overlap (1), default 4
+    iosdelay: -1, // ms to wait for the iOS webview to update before animation kicks in, default -1
+    androiddelay: -1, // same as above but for Android, default -1
+    winphonedelay: -1, // same as above but for Windows Phone, default -1,
+    fixedPixelsTop: 0, // the number of pixels of your fixed header, default 0 (iOS and Android)
+    fixedPixelsBottom: 0, // the number of pixels of your fixed footer (f.i. a tab bar), default 0 (iOS and Android)
+    triggerTransitionEvent: '$ionicView.afterEnter', // internal ionic-native-transitions option
+    backInOppositeDirection: false // Takes over default back transition and state back transition to use the opposite direction transition to go back
+  });
+
+  $ionicNativeTransitionsProvider.setDefaultTransition({
+    type: 'slide',
+    direction: 'left'
+  });
+
+  $ionicNativeTransitionsProvider.setDefaultBackTransition({
+    type: 'slide',
+    direction: 'right'
+  });
+
+  $ionicConfigProvider.scrolling.jsScrolling(false);
+
   $sceDelegateProvider.resourceUrlWhitelist([
     // Allow same origin resource loads.
     'self',
